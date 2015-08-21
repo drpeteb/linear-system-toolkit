48d3c4e1ea5261977c9984f6c96e891591891c33
Pete <pete.bunch@gmail.com>
2015-08-20 22:57:55 -0500

    Fiddling with settings

--- run_basic_learning.py
+++ run_basic_learning.py
@@ -15,9 +15,9 @@ do = 2
 
 params = dict()
 params['F'] = np.array([[0.9,0.81],[0,0.9]])
-params['Q'] = np.array([[0.1,0],[0,0.1]])
+params['Q'] = np.array([[0.1,0.05],[0.05,0.1]])
 params['H'] = np.identity(do)#np.array([[1,0]])
-params['R'] = np.identity(do)#np.array([[1]])
+params['R'] = 0.1*np.identity(do)#np.array([[1]])
 
 prior = GaussianDensity(np.array([0,0]), np.array([[100,0],[0,100]]))
 model = BasicLinearModel(ds, do, prior, params)
@@ -26,8 +26,8 @@ np.random.seed(1)
 state, observ = model.simulate_data(K)
 
 est_params = dict()
-est_params['F'] = params['F']#np.array([[0.5,0],[0,0.5]])
-est_params['Q'] = params['Q']#np.array([[1,0],[0,1]])
+est_params['F'] = np.array([[0.5,0],[0,0.5]])
+est_params['Q'] = 0.3*np.identity(ds)
 est_params['H'] = np.identity(do)#np.array([[1,0]])
 est_params['R'] = np.identity(do)#np.array([[1]])
 est_model = BasicLinearModel(ds, do, prior, est_params)
@@ -41,12 +41,12 @@ est_model = BasicLinearModel(ds, do, prior, est_params)
 
 hyperparams = dict()
 hyperparams['nu0'] = 4
-hyperparams['Psi0'] = 0.1*np.identity(ds)
+hyperparams['Psi0'] = (hyperparams['nu0']-ds-1)*np.identity(ds)
 hyperparams['M0'] = np.zeros(ds)
 hyperparams['alpha'] = 100
 learner = MCMCLearnerForBasicModelWithIndependentPriors(est_model, observ, hyperparams)
 
-num_iter = 1000
+num_iter = 500
 
 for ii in range(num_iter):
     print("Running iteration {} of {}.".format(ii+1,num_iter))
